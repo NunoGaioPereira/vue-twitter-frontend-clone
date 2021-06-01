d3dd849e4808b224a7e49bc41d8af583e1809c72
Nuno Pereira <nunocgpereira@gmail.com>
2021-06-01 21:37:32 +0100

    Add Trends section

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,32 @@
         </div>
       </div>
     </div>
+    <!-- Tweet -->
+    <div class="w-1/2 h-full">
 
+    </div>
+    <!-- Trending -->
+    <div class="w-1/3 h-full border-l border-lighter py-2 px-6 overflow-y-scroll relative">
+      <input class="pl-12 rounded-full w-full p-2 bg-lighter text-sm" placeholder="Search Twitter">
+      <i class="fas fa-search absolute left-0 top-0 mt-4 ml-12 text-sm text-light"></i>
+      <div class="w-full rounded-lg bg-lightest">
+        <div class="flex items-cent justify-between p-3">
+          <p class="text-lg font-bold">Trends for You</p>
+          <i class="fas fa-cog text-lg text-blue"></i>
+        </div>
+        <button v-for="trend in trending" :key="trend.id" class="w-full flex justify-between hover:bg-lighter p-3 border-t border-lighter">
+          <div>
+            <p class="text-sm text-left leading-tight text-dark">{{ trend.top }}</p>
+            <p class="font-bold text-left leading-tight">{{ trend.title }}</p>
+            <p class="text-left leading-tight text-dark">{{ trend.bottom }}</p>
+          </div>
+          <i class="fas fa-angle-down text-lg text-dark"></i>
+        </button>
+        <button class="w-full p-3 hover:bg-lighter text-left text-blue border-t border-lighter">
+          Show More
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -65,6 +90,13 @@ export default {
         {icon: 'fas fa-clipboard-list', title: 'Lists', id: 'lists'},
         {icon: 'far fa-user', title: 'Profile', id: 'profile'},
         {icon: 'fas fa-ellipsis-h', title: 'More', id: 'more'}
+      ],
+      trending: [
+        {top: 'Trending in TX', title: 'Gigi', bottom: 'Trending with: Rip Gigi'},
+        {top: 'Music', title: 'We Won', bottom: '135K Tweets'},
+        {top: 'Pop', title: 'Blue Ivy', bottom: '40k tweets'},
+        {top: 'Trending in US', title: 'Denim Day', bottom: '40k tweets'},
+        {top: 'Trending', title: 'When Beyonce', bottom: '25.4k tweets'},
       ],
       id: 'home',
       dropdown: false,
