89d86163c9c7c65e272597331d5fa548013ec89b
Nuno Pereira <nuno@prodrone.io>
2021-06-01 13:10:13 +0100

    Create side nav

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,38 @@
   <div id="app" class="flex container h-screen w-full">
     <!-- side nav -->
     <div class="w-1/5 border-r border-lighter px-8 py-2">
-    
+      <button class="h-12 w-12 hover:bg-lightblue text-3xl rounded-full text-blue">
+        <i class="fab fa-twitter"></i>
+      </button>
+      <div>
+        <button v-for="tab in tabs" v-bind:key="tab.id" @click="id = tab.id" :class="`hover:text-blue focus:outline-none flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3 ${ id === tab.id ? 'text-blue' : ''}`">
+            <i :class="`${ tab.icon } text-2xl mr-4`"></i>
+            <p class="text-lg font-semibold text-left">{{ tab.title }}</p>
+        </button>  
+      </div> 
     </div>
 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  component: {
     data() {
       return {
-
+        tabs: [
+          {icon: 'fas fa-home', title: 'Home', id:'home'},
+          {icon: 'fas fa-hashtag', title: 'Explore', id: 'explore'},
+          {icon: 'far fa-bell', title: 'Notifications', id: 'notifications'},
+          {icon: 'far fa-envelope', title: 'Messages', id: 'messages'},
+          {icon: 'far fa-bookmark', title: 'Bookmarks', id: 'bookmarks'},
+          {icon: 'fas fa-clipboard-list', title: 'Lists', id: 'lists'},
+          {icon: 'far fa-user', title: 'Profile', id: 'profile'},
+          {icon: 'fas fa-ellipsis-h', title: 'More', id: 'more'}
+        ],
+        id: 'home',
       }
     }
-  }
 }
 </script>
